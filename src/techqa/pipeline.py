ec2462aca29d2b6c457e9cace08366e4da1a6dd5
sarahec <sarahec@nextquestion.net>
2024-02-11 15:24:03 -0800

    Combine pipeline routines into one file, add utils

--- src/techqa/pipeline.py
+++ src/techqa/pipeline.py
@@ -1,12 +1,19 @@
-import jsonlines
 import ijson
+import jsonlines
 import math
+import os
+import spacy
+
+nlp = spacy.load('en_core_web_sm')
+assert (nlp is not None)
 
 
 def read_techqa_corpus(inpf):
     '''A generator that reads the TechQA corpus and yields documents'''
     extension = inpf.split(".")[-1]
     assert (extension in ["jsonl", "json"])
+    assert os.path.exists(inpf)
+
     if (extension == "jsonl"):
         with jsonlines.open(inpf, "r") as reader:
             for passage in reader:          # iteration avoids loading the entire file into memory
@@ -38,3 +45,39 @@ def preprocess_single_doc(passage):
             "chunk_ind": pind,
         })
     return snippets
+
+
+def run_pipeline(corpus, batchSize=1024, nProcesses=4):
+  return nlp.pipe([d["text"] for d in corpus], batch_size=batchSize, n_process=nProcesses)
+
+
+def post_process_docs(doc):
+    tokens = []
+    mentions = []
+    for token in doc:
+        if token.ent_iob_ == "B":
+            mentions.append(len(tokens))
+            tokens.append(token.text)
+            continue
+        if token.ent_iob_ == "I":
+            tokens[-1] = "{} {}".format(tokens[-1], token.text)
+            continue
+        if token.ent_iob_ == "O":
+            tokens.append(token.text)
+    yield (tokens, mentions)
+
+
+def main():
+    inpf = "data/corpus.jsonl"
+    count = 0
+    corpus = read_techqa_corpus(inpf)
+    assert corpus is not None
+    result = run_pipeline(corpus, batchSize=1024, nProcesses=2)
+    for doc in result:
+      if post_process_docs(doc) is not None:
+        count = count + 1
+    print(count)
+
+
+if __name__ == "__main__":
+    main()
