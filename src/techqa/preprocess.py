8a1ad5d2692e375bdfe8ce35e2ca0c1a6eb23460
sarahec <sarahec@nextquestion.net>
2024-03-07 11:16:05 -0800

    Full command-line support and optional disk write

--- src/techqa/preprocess.py
+++ src/techqa/preprocess.py
@@ -1,32 +1,58 @@
 import jsonlines
 import typer
 from tqdm import tqdm
+from typing import Annotated, Iterable, Dict, Optional
 from pathlib import Path
-from spacy.tokens import DocBin
+
 import spacy
+from spacy.tokens import DocBin, Doc
+from spacy import Language
 
 
-def tokenize_corpus(input_path: Path, output_path: Path):
-    spacy.prefer_gpu()
-    ref_nlp = spacy.load("en_core_web_sm")
-    nlp = spacy.blank("en", vocab=ref_nlp.vocab)
+def load_corpus(docbin_path: Path, language: Language) -> Iterable[Doc]:
+    doc_bin = DocBin().from_disk(docbin_path)
+    for doc in doc_bin.get_docs(docbin_path, language.vocab):
+        yield doc
+
+
+def tokenize_corpus(input_path: Path, language: Language, output_path: Optional[Path], batch=1024, processes=2):
+    nlp = spacy.blank("en", vocab=language.vocab)
     doc_bin = DocBin(store_user_data=True)
     with jsonlines.open(input_path) as reader:
-        json_docs = list(reader)
-    docs = nlp.pipe([(eg["text"], {"id": eg["id"], "title": eg["title"], "metadata": eg["metadata"]}) for eg in json_docs],
-                    batch_size=1024, n_process=2, as_tuples=True)
-    for doc, user_data in docs:
-        doc.user_data = user_data
-        doc_bin.add(doc)
+        # json_docs = list(reader)
+        docs = nlp.pipe([(eg["text"], {"id": eg["id"], "title": eg["title"], "metadata": eg["metadata"], "length": len(eg["text"])}) for eg in reader],
+                        batch_size=batch, n_process=processes, as_tuples=True)
+    if output_path is not None:
+        doc_bin = DocBin(store_user_data=True)
+        for doc, user_data in docs:
+            doc.user_data = user_data
+            doc_bin.add(doc)
+        doc_bin.to_disk(output_path)
+    else:
+        lines = 0
+        total_length = 0
+        for doc, user_data in docs:
+            doc.user_data = user_data
+            lines += 1
+            total_length += user_data["length"]
+        print(
+            f"Processed {lines} documents with an average length of {total_length / lines} characters")
 
-    doc_bin.to_disk(output_path)
+
+
 
 
 def main(
-    input_path: Path = typer.Argument(..., exists=True, dir_okay=False),
-    output_path: Path = typer.Argument(..., dir_okay=False),
+    input_path: Annotated[Path, typer.Argument(exists=True, dir_okay=False, help="Path to the .jsonl corpus file")],
+    output_path: Annotated[Optional[Path],
+                           typer.Argument(dir_okay=False, help="Path to the output DocBin")] = None,
+    batch: Annotated[int, typer.Option(
+        help="Batch size for processing")] = 1024,
+    processes: Annotated[int, typer.Option(
+        help="Number of processes to use for tokenization")] = 2,
 ):
-    tokenize_corpus(input_path, output_path)
+    language = spacy.load("en_core_web_sm")
+    tokenize_corpus(input_path, language, output_path, batch, processes)
 
 
 if __name__ == "__main__":
