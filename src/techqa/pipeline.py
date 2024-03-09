c8107d3a8ad09f4b3b44ce16f088eb2673f42c33
sarahec <sarahec@nextquestion.net>
2024-03-08 19:35:50 -0800

    Update pipeline w/ command line

--- src/techqa/pipeline.py
+++ src/techqa/pipeline.py
@@ -1,11 +1,12 @@
-import ijson
-import jsonlines
-import math
-import os
+from pathlib import Path
+
+from corpus.tools import label_corpus, read_corpus_entries, read_corpus_segments, save_tokenized_corpus, tokenize_corpus
+from typing import Annotated, Optional
+import tqdm
+import typer
+
 import spacy
 
-nlp = spacy.load('en_core_web_sm')
-assert (nlp is not None)
 
 def read_techqa_corpus(inpf):
     '''A generator that reads the TechQA corpus and yields documents'''
@@ -60,17 +61,28 @@ def post_process_docs(doc):
     yield (tokens, mentions)
 
 
-def main():
-    inpf = "data/corpus.jsonl"
-    count = 0
-    corpus = read_techqa_corpus(inpf)
-    assert corpus is not None
-    result = run_pipeline(corpus, batchSize=1024, nProcesses=4)
-    for doc in result:
-      if post_process_docs(doc) is not None:
-        count = count + 1
-    print(count)
-
+def main(
+    input_path: Annotated[Path, typer.Argument(exists=True, dir_okay=False, help="Path to the .jsonl corpus file")],
+    output_path: Annotated[Optional[Path],
+                           typer.Argument(dir_okay=False, help="Path to the output DocBin")] = None,
+    batch_size: Annotated[int, typer.Option(
+        help="Batch size for processing")] = 1024,
+    processes: Annotated[int, typer.Option(
+        help="Number of processes to use for NER recognition")] = 1,
+    segment_size: Annotated[int, typer.Option(
+        help="Number of words per input segment")] = None
+):
+    language = spacy.load("en_core_web_sm")
+    inputs = read_corpus_entries(
+        input_path) if segment_size is None else read_corpus_segments(input_path, segment_size)
+    docs = label_corpus(tqdm.tqdm(inputs), language, batch_size, processes)
+    if output_path is not None:
+        save_tokenized_corpus(docs, output_path)
+    else:
+        total_length = 0
+        for doc in docs:
+            total_length += len(doc)
+        print(f"Total length of corpus: {total_length}")
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
