bd3641d912874d9aaf76dc442de94a47a613a7b4
sarahec <sarahec@nextquestion.net>
2024-03-10 15:36:38 -0700

    Import unnamed

--- src/techqa_tools/__init__.py
+++ src/techqa_tools/__init__.py
@@ -1,1 +1,1 @@
-from .techqa_tools import read_corpus_entries, read_corpus_segments, load_tokenized_corpus, tokenize_corpus, save_tokenized_corpus, label_corpus
+from . import read_corpus_entries, read_corpus_segments, load_tokenized_corpus, tokenize_corpus, save_tokenized_corpus, label_corpus
