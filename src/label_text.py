955729d89159576292608a17d23adbbf1eb13371
sarahec <sarahec@nextquestion.net>
2024-03-12 12:28:47 -0700

    Benchmarking: sets, descriptive naming

--- src/label_text.py
+++ src/label_text.py
@@ -38,67 +38,79 @@ def process(language, input_path, output_path, batch_size, processes, segment_si
         print(f"Total length of corpus: {total_length}")
 
 
-def run_benchmarks(language, input_path, benchmark_path):
-    settings = [
-        {"batch_size": 1024, "processes": 1,
-            "segment_size": None, "label": "whole doc"},
-        {"batch_size": 1024, "processes": 2,
-            "segment_size": None, "label": "whole doc"},
-        {"batch_size": 1024, "processes": 3,
-            "segment_size": None, "label": "whole doc"},
-        {"batch_size": 1024, "processes": 4,
-            "segment_size": None, "label": "whole doc"},
-        {"batch_size": 1024, "processes": 5,
-            "segment_size": None, "label": "whole doc"},
-        {"batch_size": 1024, "processes": 6,
-            "segment_size": None, "label": "whole doc"},
-        {"batch_size": 1024, "processes": 1,
-            "segment_size": 64, "label": "processors"},
-        {"batch_size": 1024, "processes": 2,
-            "segment_size": 64, "label": "processors"},
-        {"batch_size": 1024, "processes": 3,
-            "segment_size": 64, "label": "processors"},
-        {"batch_size": 1024, "processes": 4,
-            "segment_size": 64, "label": "processors"},
-        {"batch_size": 1024, "processes": 5,
-            "segment_size": 64, "label": "processors"},
-        {"batch_size": 1024, "processes": 6,
-            "segment_size": 64, "label": "processors"},
-        {"batch_size": 1024, "processes": 4,
-            "segment_size": 128, "label": "segment size"},
-        {"batch_size": 1024, "processes": 4,
-            "segment_size": 256, "label": "segment size"},
-        {"batch_size": 1024, "processes": 4,
-            "segment_size": 512, "label": "segment size"},
-        {"batch_size": 64, "processes": 4,
-            "segment_size": 512, "label": "batch size"},
-        {"batch_size": 32, "processes": 4,
-            "segment_size": 512, "label": "batch size"},
-        {"batch_size": 16, "processes": 4,
-            "segment_size": 512, "label": "batch size"},
-        {"batch_size": 12, "processes": 4,
-            "segment_size": 512, "label": "batch size"},
-        {"batch_size": 8, "processes": 4,
-            "segment_size": 512, "label": "batch size"},
-        {"batch_size": 4, "processes": 4,
-            "segment_size": 512, "label": "batch size"},
+benchmark_groups = ["doc", "processors", "segment", "batch"]
 
-    ]
+
+def run_benchmarks(language: spacy.Language, input_path: Path, benchmark_path: Path, select="all", treatment: str = 'unknown', use_gpu: bool = False):
+    benchmarks = {"doc": [
+        {"batch_size": 1024, "processes": 1,
+            "segment_size": None, "label": "doc"},
+        {"batch_size": 1024, "processes": 2,
+            "segment_size": None, "label": "doc"},
+        {"batch_size": 1024, "processes": 3,
+            "segment_size": None, "label": "doc"},
+        {"batch_size": 1024, "processes": 4,
+            "segment_size": None, "label": "doc"},
+        {"batch_size": 1024, "processes": 5,
+            "segment_size": None, "label": "doc"},
+        {"batch_size": 1024, "processes": 6,
+            "segment_size": None, "label": "doc"},
+    ],
+        "processors": [
+            {"batch_size": 1024, "processes": 1,
+             "segment_size": 64, "label": "processors"},
+            {"batch_size": 1024, "processes": 2,
+             "segment_size": 64, "label": "processors"},
+            {"batch_size": 1024, "processes": 3,
+             "segment_size": 64, "label": "processors"},
+            {"batch_size": 1024, "processes": 4,
+             "segment_size": 64, "label": "processors"},
+            {"batch_size": 1024, "processes": 5,
+             "segment_size": 64, "label": "processors"},
+            {"batch_size": 1024, "processes": 6,
+             "segment_size": 64, "label": "processors"},
+    ],
+        "segment": [
+        {"batch_size": 1024, "processes": 4,
+            "segment_size": 128, "label": "segment"},
+        {"batch_size": 1024, "processes": 4,
+            "segment_size": 256, "label": "segment"},
+        {"batch_size": 1024, "processes": 4,
+            "segment_size": 512, "label": "segment"},
+    ],
+        "batch": [
+        {"batch_size": 64, "processes": 4,
+            "segment_size": 512, "label": "batch"},
+        {"batch_size": 32, "processes": 4,
+            "segment_size": 512, "label": "batch"},
+        {"batch_size": 16, "processes": 4,
+            "segment_size": 512, "label": "batch"},
+        {"batch_size": 12, "processes": 4,
+            "segment_size": 512, "label": "batch"},
+        {"batch_size": 8, "processes": 4,
+            "segment_size": 512, "label": "batch"},
+        {"batch_size": 4, "processes": 4,
+            "segment_size": 512, "label": "batch"},
+    ]}
+
+    selected = benchmarks[select] if select in benchmark_groups else benchmarks["doc"] + \
+        benchmarks["processors"] + benchmarks["segment"] + benchmarks["batch"]
+
     print(f"Running benchmarks")
     with open(benchmark_path, 'w', newline='') as csvfile:
-        fieldnames = ['label', 'time', 'processes',
-                      'segment_size', 'batch_size']
+        fieldnames = ["group", "time", "processes",
+                      "segment_size", "batch_size", "gpu", "treatment"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
 
-        for setting in tqdm.tqdm(settings):
-            t0 = time.time()
+        for setting in tqdm.tqdm(selected):
+            t0 = time.perf_counter()
             process(language, input_path, None,
                     setting["batch_size"], setting["processes"], setting["segment_size"])
-            t1 = time.time()
+            t1 = time.perf_counter()
             total = t1 - t0
             writer.writerow(
-                {'label': setting["label"], 'time': total, 'processes': setting["processes"], 'segment_size': setting["segment_size"], 'batch_size': setting["batch_size"]})
+                {"group": setting["label"], "time": total, "processes": setting["processes"], "segment_size": setting["segment_size"], "batch_size": setting["batch_size"], "gpu": use_gpu, "treatment": treatment})
 
 
 def main(
@@ -111,10 +123,10 @@ def main(
         help="Number of processes to use for NER recognition")] = 1,
     segment_size: Annotated[int, typer.Option(
         help="Number of words per input segment")] = None,
-    benchmark: Annotated[bool, typer.Option(
-        help="Run a benchmark for different settings")] = False,
-    benchmark_path: Annotated[Optional[Path], typer.Option(
-        help="Path to the benchmark output file")] = "benchmarks/label_text.csv",
+    benchmark: Annotated[str, typer.Option(
+        help="Run a benchmark: all, doc, procesors, segment, or batch")] = None,
+    treatment: Annotated[Optional[str], typer.Option(
+        help="Name of the treatment applied")] = "unknown",
     use_gpu: Annotated[bool, typer.Option(
         help="Use GPU for processing")] = False,
 
@@ -123,7 +135,10 @@ def main(
         spacy.prefer_gpu()
     language = spacy.load("en_core_web_sm")
     if benchmark:
-        run_benchmarks(language, input_path, benchmark_path)
+        gpu_label = "-gpu" if use_gpu else ""
+        benchmark_path = f"benchmarks/label_text-{benchmark}-{treatment}{gpu_label}.csv"
+        run_benchmarks(language, input_path, benchmark_path,
+                       benchmark, treatment, use_gpu)
     else:
         process(language, input_path, output_path,
                 batch_size, processes, segment_size)
